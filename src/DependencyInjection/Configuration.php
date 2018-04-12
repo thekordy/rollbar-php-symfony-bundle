f235668d2da3f7a53ef2e09b8e1f9930d7701505
Artur Moczulski <artur.moczulski@gmail.com>
2018-04-12 22:19:09 +0000

    composer fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -29,33 +29,33 @@ class Configuration implements ConfigurationInterface
             $rollbarConfigNode = $rootNode->children()
                 ->arrayNode('rollbar');
             
-                foreach (\Rollbar\Config::listOptions() as $option) {
+        foreach (\Rollbar\Config::listOptions() as $option) {
+            // TODO: this is duplicated code from
+            // https://github.com/rollbar/rollbar-php-wordpress/blob/master/src/Plugin.php#L359-L366
+            // It needs to get replaced with a native rollbar/rollbar-php method
+            // as pointed out here https://github.com/rollbar/rollbar-php/issues/344
+            $method = lcfirst(str_replace('_', '', ucwords($option, '_')));
                     
-                    // TODO: this is duplicated code from https://github.com/rollbar/rollbar-php-wordpress/blob/master/src/Plugin.php#L359-L366
-                    // It needs to get replaced with a native rollbar/rollbar-php method
-                    // as pointed out here https://github.com/rollbar/rollbar-php/issues/344
-                    $method = lcfirst(str_replace('_', '', ucwords($option, '_')));
+            // Handle the "branch" exception
+            switch ($method) {
+                case "branch":
+                    $method = "gitBranch";
+                    break;
+                case "includeErrorCodeContext":
+                    $method = 'includeCodeContext';
+                    break;
+                case "includeExceptionCodeContext":
+                    $method = 'includeExcCodeContext';
+                    break;
+            }
                     
-                    // Handle the "branch" exception
-                    switch($method) {
-                        case "branch":
-                            $method = "gitBranch";
-                            break;
-                        case "includeErrorCodeContext":
-                            $method = 'includeCodeContext';
-                            break;
-                        case "includeExceptionCodeContext":
-                            $method = 'includeExcCodeContext';
-                            break;
-                    }
+            $default = method_exists(\Rollbar\Defaults::get(), $method) ?
+            \Rollbar\Defaults::get()->$method() :
+            null;
                     
-                    $default = method_exists(\Rollbar\Defaults::get(), $method) ?
-                        \Rollbar\Defaults::get()->$method() :
-                        null;
-                    
-                    $rollbarConfigNode->children()
-                        ->scalarNode($option)->defaultValue($default)->end();
-                }
+            $rollbarConfigNode->children()
+            ->scalarNode($option)->defaultValue($default)->end();
+        }
                 
             $rollbarConfigNode->end();
         
