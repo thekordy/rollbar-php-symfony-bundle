27392f11fd186d17a8de58083db140ba4b325fad
Artur Moczulski <artur.moczulski@gmail.com>
2018-04-14 00:57:33 +0000

    github-9: build expected default values dyncamically from the config class

--- tests/SymfonyRollbarBundle/DependencyInjection/ConfigurationTest.php
+++ tests/SymfonyRollbarBundle/DependencyInjection/ConfigurationTest.php
@@ -18,39 +18,39 @@ class ConfigurationTest extends KernelTestCase
         $container = static::$kernel->getContainer();
 
         $config           = $container->getParameter(SymfonyRollbarExtension::ALIAS . '.config');
-        $defaultErrorMask = E_ERROR | E_WARNING | E_PARSE | E_CORE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR;
-
+        
+        $defaults = [];
+        foreach (\Rollbar\Config::listOptions() as $option) {
+            // TODO: this is duplicated code from
+            // https://github.com/rollbar/rollbar-php-wordpress/blob/master/src/Plugin.php#L359-L366
+            // It needs to get replaced with a native rollbar/rollbar-php method
+            // as pointed out here https://github.com/rollbar/rollbar-php/issues/344
+            $method = lcfirst(str_replace('_', '', ucwords($option, '_')));
+                    
+            // Handle the "branch" exception
+            switch ($method) {
+                case "branch":
+                    $method = "gitBranch";
+                    break;
+                case "includeErrorCodeContext":
+                    $method = 'includeCodeContext';
+                    break;
+                case "includeExceptionCodeContext":
+                    $method = 'includeExcCodeContext';
+                    break;
+            }
+                    
+            $default = method_exists(\Rollbar\Defaults::get(), $method) ?
+                \Rollbar\Defaults::get()->$method() :
+                null;
+                    
+            $defaults[$option] = $default;
+        }
+        
+        
         $default = [
             'enable' => true,
-            'rollbar' => [
-                'access_token'                   => null,
-                'agent_log_location'             => static::$kernel->getLogDir() . '/rollbar.log',
-                'base_api_url'                   => 'https://api.rollbar.com/api/1/',
-                'batch_size'                     => Configuration::BATCH_SIZE,
-                'batched'                        => false,
-                'branch'                         => Configuration::BRANCH,
-                'capture_error_stacktraces'      => true,
-                'checkIgnore'                    => null,
-                'code_version'                   => '',
-                'enable_utf8_sanitization'       => true,
-                'environment'                    => Configuration::ENVIRONMENT,
-                'error_sample_rates'             => [],
-                'handler'                        => Configuration::HANDLER_BLOCKING,
-                'blocking'                       => null,
-                'include_error_code_context'     => false,
-                'include_exception_code_context' => false,
-                'included_errno'                 => $defaultErrorMask,
-                'logger'                         => null,
-                'person'                         => [],
-                'person_fn'                      => null,
-                'root'                           => static::$kernel->getRootDir(),
-                'scrub_fields'                   => Configuration::$scrubFieldsDefault,
-                'shift_function'                 => true,
-                'timeout'                        => 3,
-                'report_suppressed'              => false,
-                'use_error_reporting'            => false,
-                'proxy'                          => null,
-            ],
+            'rollbar' => $defaults
         ];
 
         $this->assertNotEmpty($config);
